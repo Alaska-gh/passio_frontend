5ec472abd2ae59846fa430ca339faa8a0fcf9c95
Alaska-gh <bashiruyussif@gmail.com>
2026-04-13 18:19:45 +0000

    setup firebase dependencies and configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeng/themes/lara';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,7 +25,10 @@ export const appConfig: ApplicationConfig = {
           darkModeSelector: false,
         },
       },
-    }),
+    }), provideFirebaseApp(() => initializeApp({ projectId: "passio-945fd", appId: "1:36645642297:web:c38a95f33bd34074e7ede8", storageBucket: "passio-945fd.firebasestorage.app", apiKey: "AIzaSyDr4BIkw9jyw2QqMXhBWXdz8XgmzupM9R8", authDomain: "passio-945fd.firebaseapp.com", messagingSenderId: "36645642297", projectNumber: "36645642297", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
 // "paths": {
